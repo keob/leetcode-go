72831452f54b7e2d7694846b97614ab5d60bdfe4
keob <isgopher@outlook.com>
2020-07-08 20:00:44 +0800

    Add count primes solution

---
+++ solution204/solution.go
@@ -0,0 +1,22 @@
+package solution204
+
+func countPrimes(n int) int {
+	count := 0
+	signs := make([]bool, n)
+
+	if n <= 2 {
+		return 0
+	}
+
+	for i := 2; i < n; i++ {
+		if signs[i] {
+			continue
+		}
+		count++
+		for j := 2 * i; j < n; j += i {
+			signs[j] = true
+		}
+	}
+
+	return count
+}
---
+++ solution204/solution_test.go
@@ -0,0 +1,23 @@
+package solution204
+
+import "testing"
+
+func TestCountPrimes(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want int
+	}{
+		{"case 1", 10, 4},
+		{"case 2", 0, 0},
+		{"case 3", 100, 25},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPrimes(tt.n); got != tt.want {
+				t.Errorf("countPrimes() = %v wang %v", got, tt.want)
+			}
+		})
+	}
+}
