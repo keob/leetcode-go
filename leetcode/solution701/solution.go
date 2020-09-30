4bd945e0e8eb4df8f87a430426ea813a8999a921
keob <isgopher@outlook.com>
2020-09-30 19:57:51 +0800

    Add insert into a binary search tree solution

---
+++ leetcode/solution701/solution.go
@@ -0,0 +1,32 @@
+package solution701
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	p := root
+
+	for p != nil {
+		if val < p.Val {
+			if p.Left == nil {
+				p.Left = &TreeNode{Val: val}
+				break
+			}
+			p = p.Left
+		} else {
+			if p.Right == nil {
+				p.Right = &TreeNode{Val: val}
+				break
+			}
+			p = p.Right
+		}
+	}
+	return root
+}
