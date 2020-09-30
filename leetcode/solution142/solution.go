c1d952e39ca1962cef526c5dda67998880f6b885
keob <isgopher@outlook.com>
2020-09-30 20:12:58 +0800

    Add linked list cycle ii solution

---
+++ leetcode/solution142/solution.go
@@ -0,0 +1,35 @@
+package solution142
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	tmp := head
+
+	for tmp != slow {
+		tmp = tmp.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
