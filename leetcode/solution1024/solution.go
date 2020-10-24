5b4091dac1f69373526b63eff5d5b469104b19df
keob <isgopher@outlook.com>
2020-10-24 18:53:29 +0800

    Add video stitching solution

---
+++ leetcode/solution1024/solution.go
@@ -0,0 +1,38 @@
+package solution1024
+
+func videoStitching(clips [][]int, T int) int {
+	count := 0
+
+	for end, index := 0, len(clips)-1; end < T; count++ {
+		index, end = nextInterval(clips, index, end)
+
+		if end == -1 {
+			count = -1
+			break
+		}
+	}
+
+	return count
+}
+
+func nextInterval(src [][]int, index, limit int) (int, int) {
+	end := limit
+
+	for i := 0; i <= index; i++ {
+		if src[i][0] <= limit {
+			if src[i][1] > end {
+				end = src[i][1]
+			}
+
+			src[i], src[index] = src[index], src[i]
+			index--
+			i--
+		}
+	}
+
+	if end == limit {
+		end = -1
+	}
+
+	return index, end
+}
