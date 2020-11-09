95a03e63baddd8770d66c569d43ceced97a1dbe8
keob <isgopher@outlook.com>
2020-11-09 23:20:01 +0800

    Add k closest points to origin solution

---
+++ leetcode/solution973/solution.go
@@ -0,0 +1,12 @@
+package solution973
+
+import "sort"
+
+func kClosest(points [][]int, K int) [][]int {
+	sort.Slice(points, func(i, j int) bool {
+		p, q := points[i], points[j]
+		return p[0]*p[0]+p[1]*p[1] < q[0]*q[0]+q[1]*q[1]
+	})
+
+	return points[:K]
+}
