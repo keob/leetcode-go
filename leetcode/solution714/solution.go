4e469f52657bcc634506b735b22d8118335b55ae
keob <isgopher@outlook.com>
2020-12-17 19:21:38 +0800

    Add best time to buy and sell stock with transaction fee solution

---
+++ leetcode/solution714/solution.go
@@ -0,0 +1,18 @@
+package solution714
+
+func maxProfit(prices []int, fee int) int {
+	n := len(prices)
+	buy := prices[0] + fee
+	profit := 0
+
+	for i := 0; i < n; i++ {
+		if prices[i]+fee < buy {
+			buy = prices[i] + fee
+		} else if prices[i] > buy {
+			profit += prices[i] - buy
+			buy = prices[i]
+		}
+	}
+
+	return profit
+}
