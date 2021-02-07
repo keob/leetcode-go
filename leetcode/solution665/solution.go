a07455d315cc8b4555eb06b51d35df78af5cc89b
keob <isgopher@outlook.com>
2021-02-07 19:41:44 +0800

    Add non decreasing array solution

---
+++ leetcode/solution665/solution.go
@@ -0,0 +1,20 @@
+package solution665
+
+func checkPossibility(nums []int) bool {
+	cnt := 0
+
+	for i := 0; i < len(nums)-1; i++ {
+		x, y := nums[i], nums[i+1]
+		if x > y {
+			cnt++
+			if cnt > 1 {
+				return false
+			}
+			if i > 0 && y < nums[i-1] {
+				nums[i+1] = x
+			}
+		}
+	}
+
+	return true
+}
