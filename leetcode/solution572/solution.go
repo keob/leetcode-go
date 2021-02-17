859a291a04cb2bbe108817a97fd95fb8000f6bb4
keob <isgopher@outlook.com>
2021-02-17 19:33:43 +0800

    Add subtree of another tree solution

---
+++ leetcode/solution572/solution.go
@@ -0,0 +1,31 @@
+package solution572
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+
+	return check(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func check(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if a.Val == b.Val {
+		return check(a.Left, b.Left) && check(a.Right, b.Right)
+	}
+
+	return false
+}
