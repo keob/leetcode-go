6a357ef54fe7bf7f369c759a050126ab3d1508d8
kebo <kebo2021@qq.com>
2021-03-01 18:25:42 +0800

    Add range sum query immutable solution

---
+++ leetcode/solution303/solution.go
@@ -0,0 +1,19 @@
+package solution303
+
+type NumArray struct {
+	sums []int
+}
+
+func Constructor(nums []int) NumArray {
+	sums := make([]int, len(nums)+1)
+
+	for i, v := range nums {
+		sums[i+1] = sums[i] + v
+	}
+
+	return NumArray{sums}
+}
+
+func (na *NumArray) SumRange(i int, j int) int {
+	return na.sums[j+1] - na.sums[i]
+}
