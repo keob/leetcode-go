612ae344f46e9a0883c89aa987814a332ac9ea81
kebo <kebo2021@qq.com>
2021-03-30 18:17:48 +0800

    Add search a 2d matrix solution

---
+++ leetcode/solution74/solution.go
@@ -0,0 +1,13 @@
+package solution74
+
+import "sort"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+
+	i := sort.Search(m*n, func(i int) bool {
+		return matrix[i/n][i%n] >= target
+	})
+
+	return i < m*n && matrix[i/n][i%n] == target
+}
