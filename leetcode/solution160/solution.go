8560c941aaaf0eb7f6efd88eeb684140168a9875
kebo <kebo2021@qq.com>
2021-06-04 18:26:57 +0800

    Add intersection of two linked lists solution

---
+++ leetcode/solution160/solution.go
@@ -0,0 +1,29 @@
+package solution160
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	ha, hb := headA, headB
+
+	for ha != hb {
+		if ha == nil {
+			ha = headB
+		} else {
+			ha = ha.Next
+		}
+		if hb == nil {
+			hb = headA
+		} else {
+			hb = hb.Next
+		}
+	}
+
+	return ha
+}
