9ada8b4e934d3cadcc457b7731c8ba5c06b0f59a
kebo <kebo2021@qq.com>
2021-08-27 21:43:11 +0800

    Add find median from data stream solution

---
+++ leetcode/solution295/solution.go
@@ -0,0 +1,51 @@
+package solution295
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type hp struct{ sort.IntSlice }
+
+func (h *hp) Push(v interface{}) {
+	h.IntSlice = append(h.IntSlice, v.(int))
+}
+
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+
+type MedianFinder struct {
+	queMin hp
+	queMax hp
+}
+
+func Constructor() MedianFinder {
+	return MedianFinder{}
+}
+
+func (mf *MedianFinder) AddNum(num int) {
+	minQ, maxQ := &mf.queMin, &mf.queMax
+	if minQ.Len() == 0 || num <= -minQ.IntSlice[0] {
+		heap.Push(minQ, -num)
+		if maxQ.Len()+1 < minQ.Len() {
+			heap.Push(maxQ, -heap.Pop(minQ).(int))
+		}
+	} else {
+		heap.Push(maxQ, num)
+		if maxQ.Len() > minQ.Len() {
+			heap.Push(minQ, -heap.Pop(maxQ).(int))
+		}
+	}
+}
+
+func (mf *MedianFinder) FindMedian() float64 {
+	minQ, maxQ := mf.queMin, mf.queMax
+	if minQ.Len() > maxQ.Len() {
+		return float64(-minQ.IntSlice[0])
+	}
+	return float64(maxQ.IntSlice[0]-minQ.IntSlice[0]) / 2
+}
