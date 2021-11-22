66555b7141fcb523b4f796fc5dc5fca392436bfa
kebo <kebo2021@qq.com>
2021-11-22 19:42:43 +0800

    Add shuffle an array solution

---
+++ leetcode/solution384/solution.go
@@ -0,0 +1,26 @@
+package solution384
+
+import "math/rand"
+
+type Solution struct {
+	nums     []int
+	original []int
+}
+
+func Constructor(nums []int) Solution {
+	return Solution{nums, append([]int(nil), nums...)}
+}
+
+func (s *Solution) Reset() []int {
+	copy(s.nums, s.original)
+	return s.nums
+}
+
+func (s *Solution) Shuffle() []int {
+	n := len(s.nums)
+	for i := range s.nums {
+		j := i + rand.Intn(n-i)
+		s.nums[i], s.nums[j] = s.nums[j], s.nums[i]
+	}
+	return s.nums
+}
